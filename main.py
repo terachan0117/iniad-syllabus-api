eae29722b88268dc7c54529bdf67e5c182ae6647
Terachan <contact@tera-chan.com>
2020-09-11 21:28:41 +0900

    Delete unnecessary characters

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import re
 import json
 
 # 取得するシラバスの年度
-year = datetime.datetime.now().strftime("%Y")"
+year = datetime.datetime.now().strftime("%Y")
 
 # Seleniumオプション設定
 options = webdriver.ChromeOptions()
